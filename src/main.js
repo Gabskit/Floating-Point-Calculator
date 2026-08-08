17194a85f91dd02a9199f075260736680497b3c9
Gabskit <gaby.mofa31@gmail.com>
2026-08-07 18:53:43 -0600

    Inicio pagina

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // main.js
 import './style.css';
+import './Jqmstyle.css'
 import 'flyonui/flyonui'; // Loads FlyonUI's interactive elements
 import Alpine from 'alpinejs';
 
@@ -8,7 +9,15 @@ window.Alpine = Alpine;
 Alpine.start();
 
 document.querySelector('#app').innerHTML = `
-
+<div x-data="{}">
+	<div class="ui-bar-d bar">
+		<h1>Floating Point calculator</h1>
+		<button class="btn glass squircle-cor"><span class="icon-[solar--hamburger-menu-line-duotone]"></span></button>
+	</div>
+	<div class="content-sp">
+		<div class=""></div>
+	</div>
+</div>
 `
 
 setupCounter(document.querySelector('#counter'))
